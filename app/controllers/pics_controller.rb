0e40531aeac0ca8a3583133f18d472c502e96e0b
NadimRai <rai_nadim@yahoo.com>
2016-11-08 19:23:05 -0800

    Edit, update and destroy actions for Pic

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,7 @@
 class PicsController < ApplicationController
+	before_action :find_pic, only:[:show, :edit, :update, :destroy]
 	def index
+		@pics = Pic.all.order("created_at DESC")
 	end
 
 	def new
@@ -8,12 +10,42 @@ class PicsController < ApplicationController
 
 	def create
 		@pic = Pic.new(pic_params)
+		if @pic.save
+			redirect_to @pic, notice: "Yesss! It is posted"
+		else
+			render 'new'
+		end
 	end
+
+	def show 
+		
+	end
+	
+	def edit
+	end
+
+	def update
+		if @pic.update(pic_params)
+			redirect_to @pic, notice: "Yess! It was updated"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@pic.destroy
+		redirect_to root_path
+	end
+		
+	
 
 	private
 
 	def pic_params
-		params.require(:pic).permit(:title, :body)
+		params.require(:pic).permit(:title, :description)
 	end
 
+	def find_pic
+		@pic = Pic.find(params[:id])
+	end
 end
